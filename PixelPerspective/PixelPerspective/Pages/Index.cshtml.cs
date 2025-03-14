c45a244d11a8e425509d05988bd5cd63feb96dd6
calebm01 <calebrmouritsen@gmail.com>
2025-03-14 01:33:46 -0600

    Updated frontend and backend of home page to support game data in the DB

--- PixelPerspective/PixelPerspective/Pages/Index.cshtml.cs
+++ PixelPerspective/PixelPerspective/Pages/Index.cshtml.cs
@@ -1,21 +1,60 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using IGDB.Models;
+using Microsoft.AspNetCore.Identity;
+using PixelPerspective.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace PixelPerspective.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly PixelPerspectiveContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, PixelPerspectiveContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public void OnGet()
-        {
+        public List<PixelPerspective.Models.Game> TopBar { get; set; } = new List<Models.Game>();
+        public List<PixelPerspective.Models.Game> NewReleases { get; set; } = new List<Models.Game>();
+        public List<PixelPerspective.Models.Game> GamesOfTheYear { get; set; } = new List<Models.Game>();
+        public List<PixelPerspective.Models.Game> TrendingGames { get; set; } = new List<Models.Game>();
+        public List<PixelPerspective.Models.Game> OurTopPicks { get; set; } = new List<Models.Game>();
 
+
+        public async Task OnGetASync()
+        {
+            //implementing random element
+            var rnd = new Random();
+
+            //Get Top Bar games
+            TopBar = await _context.Game
+                .OrderBy(x => rnd.Next())
+                .Take(8)
+                .ToListAsync();
+
+            //Get New Releases
+            NewReleases = await _context.Game
+                .Take(5)
+                .ToListAsync();
+
+            //Get Game of the Year nominees
+            GamesOfTheYear = await _context.Game
+                .Take(5)
+                .ToListAsync();
+
+            //Get trending games
+            TrendingGames = await _context.Game
+                .Take(5)
+                .ToListAsync();
+
+            //Get our top picks
+            OurTopPicks = await _context.Game
+                .Take(5)
+                .ToListAsync();
         }
     }
 }
