06052ef96be94d06c14456700cc588c475be1ef0
calebm01 <calebrmouritsen@gmail.com>
2025-04-12 06:53:50 -0600

    Add Friend logic on user profile implemented

--- PixelPerspective/PixelPerspective/Pages/UserProfile.cshtml.cs
+++ PixelPerspective/PixelPerspective/Pages/UserProfile.cshtml.cs
@@ -4,19 +4,26 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using PixelPerspective.Areas.Identity.Data;
 using PixelPerspective.Models;
+using PixelPerspective.Data;
 
 namespace PixelPerspective.Pages
 {
     public class UserProfileModel : PageModel
     {
         private readonly UserManager<PixelPerspectiveUser> _userManager;
+        private readonly ILogger<IndexModel> _logger;
+        private readonly PixelPerspectiveContext _context;
 
-        public UserProfileModel(UserManager<PixelPerspectiveUser> userManager)
+        public UserProfileModel(ILogger<IndexModel> logger, UserManager<PixelPerspectiveUser> userManager, PixelPerspectiveContext context)
         {
+            _logger = logger;
             _userManager = userManager;
+            _context = context;
         }
 
         public PixelPerspectiveUser user { get; set; }
+
+        public PixelPerspectiveUser currentuser { get; set; }
         public List<Game> GameLibrary { get; set; }
 
         public async Task<IActionResult> OnGetAsync(string displayName)
@@ -25,6 +32,8 @@ namespace PixelPerspective.Pages
             {
                 return NotFound();
             }
+
+            currentuser = await _userManager.GetUserAsync(User);
 
             user = await _userManager.Users.FirstOrDefaultAsync(u => u.DisplayName == displayName);
             
