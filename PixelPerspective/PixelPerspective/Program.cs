b483a10db6758487f9acac935f29bd3c045fa098
Jordan Stewart <121924540+jstewart4u2c@users.noreply.github.com>
2025-04-22 20:55:14 -0600

    Defaulted to Local DB

--- PixelPerspective/PixelPerspective/Program.cs
+++ PixelPerspective/PixelPerspective/Program.cs
@@ -5,7 +5,7 @@ using PixelPerspective.Areas.Identity.Data;
 
 
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("PixelPerspectiveContextConnection") ?? throw new InvalidOperationException("Connection string 'PixelPerspectiveContextConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'PixelPerspectiveContextConnection' not found.");
 
 builder.Services.AddDbContext<PixelPerspectiveContext>(options => options.UseSqlServer(connectionString));
 
